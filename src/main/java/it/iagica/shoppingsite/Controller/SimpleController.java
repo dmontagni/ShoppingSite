a5d3d79ef009c2885dfc42eeb9926abecf40905b
Luca <lucaricci90@gmail.com>
2021-11-10 17:22:09 +0100

    versione test luca mohamed marco da finire!!!

--- src/main/java/it/iagica/shoppingsite/Controller/SimpleController.java
+++ src/main/java/it/iagica/shoppingsite/Controller/SimpleController.java
@@ -1,4 +1,70 @@
 package it.iagica.shoppingsite.Controller;
 
+import it.iagica.shoppingsite.Model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+
+@Controller
 public class SimpleController {
+    List<User> usersList;
+@GetMapping("/")
+    public static String showHome(Model model){
+    model.addAttribute("user", new User());
+    return "login";
+}
+@GetMapping("/register")
+    public String showRegistration(Model model){
+    User users  = new User();
+    model.addAttribute("user", users);
+    return "register";
+}
+
+@PostMapping("/register")
+    public String salvaUser(@ModelAttribute User user , Model model){
+    model.addAttribute("user",user);
+    if(usersList.add(user)){
+        model.addAttribute("user", user);
+        return "index";
+    } else{
+        return "saveError";
+    }
+
+
+}
+//provvisiorio
+    @PostMapping("/login")
+    public String loginUser(@ModelAttribute User user , Model model){
+        model.addAttribute("user",user);
+        if(getUtente(usersList,user)!= null){
+            model.addAttribute("user", user);
+
+            return "result";
+        } else{
+            return "saveError";
+        }
+    }
+
+
+
+// Metodo per analizzare la lista e cerca user se corrisponde con user all' interno della lista
+    public  User getUtente (List<User> usersList , User user){
+        for (User utente: usersList
+             ) {
+            if (utente.getUsername().equals(user.getUsername()) && utente.getPassword().equals(user.getPassword())){
+                return utente;
+            }
+
+        }
+        return null;
+    }
+
+
+
+
 }
